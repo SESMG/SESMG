a69fa6bd1bbd8d58f14e119767bbf2ad12207200
GregorBecker <63057113+GregorBecker@users.noreply.github.com>
2021-01-28 11:02:36 +0100

    Bug fix
    Invalid Syntax while building documentation

--- setup.py
+++ setup.py
@@ -31,23 +31,24 @@ setup(
     classifiers=[
         # complete classifier list:
         # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    python_requires='3.7',
-    install_requires=[
-        pandas==0.25.3
-        numpy==1.17.4
-        tables==3.5.2
-        openpyxl==3.0.0
-        https://github.com/oemof/oemof-solph/archive/master.zip
-        oemof.thermal==0.0.3
-        demandlib==0.1.6
-        pvlib==0.7.1
-        feedinlib==0.0.12
-        richardsonpy==0.2.1
-        dash==1.7.0
-        dash_canvas==0.1.0
-        pydot==1.4.1
-        graphviz==0.13.2
-        xlrd==1.2.0
+        python_requires='>=3.7',
+        install_requires=[
+            pandas==0.25.3
+            numpy==1.17.4
+            tables==3.5.2
+            openpyxl==3.0.0
+            https://github.com/oemof/oemof-solph/archive/master.zip
+            oemof.thermal==0.0.3
+            demandlib==0.1.6
+            pvlib==0.7.1
+            feedinlib==0.0.12
+            richardsonpy==0.2.1
+            dash==1.7.0
+            dash_canvas==0.1.0
+            pydot==1.4.1
+            graphviz==0.13.2
+            xlrd==1.2.0
+            ],
     ],
     extras_require={
         "dev": ["pytest", "sphinx", "sphinx_rtd_theme"],
