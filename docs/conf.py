35251ba68fdbc27126fb0d347b0f65a708077089
GregorBecker <63057113+GregorBecker@users.noreply.github.com>
2021-06-23 08:09:33 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -45,3 +45,6 @@ html_short_title = '%s-%s' % (project, version)
 napoleon_use_ivar = True
 napoleon_use_rtype = False
 napoleon_use_param = False
+
+def setup(app):
+        app.add_stylesheet('custom.css')
