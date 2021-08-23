de29f06400648cd16a15a4e2eaeac45d84df23fe
GregorBecker <63057113+GregorBecker@users.noreply.github.com>
2021-08-23 16:24:32 +0200

    bug fix

--- start_script.py
+++ start_script.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     if sys.platform.startswith("darwin"):
         import program_files.GUI
     else:
-        subprocess.call("Scripts/python3 program_files/GUI.py")
+        subprocess.call("Scripts/python program_files/GUI.py")
