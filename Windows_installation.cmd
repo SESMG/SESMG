8cfc12d9969b58e78559934a1be0f42d79895e1c
Gregor Becker <63057113+GregorBecker@users.noreply.github.com>
2021-09-21 09:15:33 +0200

    Update Windows_installation.cmd

--- Windows_installation.cmd
+++ Windows_installation.cmd
@@ -29,6 +29,9 @@ pip install graphviz==0.13.2
 pip install xlrd==1.2.0
 pip install Pyomo==5.7.1
 pip install xlsxwriter
+pip install pyproj
+pip install sympy
+pip install dhnx
 
 @echo ######################
 @echo Installation completed
