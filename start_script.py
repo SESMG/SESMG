64233c4b8e4a1f8b94765c5888e83ef83646c19d
Gregor Becker <63057113+GregorBecker@users.noreply.github.com>
2021-10-06 09:41:49 +0200

    Enable Linux usage

--- start_script.py
+++ start_script.py
@@ -3,5 +3,7 @@ import subprocess
 if __name__ == "__main__":
     if sys.platform.startswith("darwin"):
         import program_files.GUI
+    elif sys.platform.startswith("linux"):
+        import program_files.GUI
     else:
         subprocess.call("Scripts/python program_files/GUI.py")
