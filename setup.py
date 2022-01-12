ca8616ee2b175f949de737890d6f5cd4533f6ee7
Gregor Becker <63057113+GregorBecker@users.noreply.github.com>
2022-01-12 07:11:17 +0100

    Fix RTD

--- setup.py
+++ setup.py
@@ -31,15 +31,20 @@ setup(
     classifiers=[
         # complete classifier list:
         # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-            ],
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'],
 
     install_requires=[
-        "pillow>=6.2.0",
-        "pandas==1.1",
-        "numpy==1.19",
+        "scipy==1.6.3",
+        "pillow==8.3.2",
+        "pandas==1.1.0",
+        "numpy==1.19.0",
         "tables==3.5.2",
         "openpyxl==3.0.0",
         "oemof.solph==0.4",
+        "oemof.network==0.4.0rc0",
+        "oemof.thermal==0.0.5",
         "demandlib==0.1.8",
         "pvlib==0.7.1",
         "feedinlib==0.0.12",
@@ -49,10 +54,14 @@ setup(
         "pydot==1.4.1",
         "graphviz==0.13.2",
         "xlrd==1.2.0",
-        "Pyomo==5.7.1"
+        "Pyomo==5.7.1",
+        "sympy==1.9",
+        "pyproj==3.2.1",
+        "folium==0.12.1.post1",
+        "addict==2.4.0",
+        "xlsxwriter==3.0.2"
         ],
     extras_require={
-        "dev": ["pytest", "sphinx", "sphinx_rtd_theme","oemof.thermal"],
+        "dev": ["pytest", "sphinx", "sphinx_rtd_theme"],
     },  
 )
-
