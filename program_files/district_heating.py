2bcdb7c6cae661f8910ac9f12853b4b6cb0b0a6f
Gregor Becker <63057113+GregorBecker@users.noreply.github.com>
2022-02-20 19:47:24 +0100

    Update district_heating.py

--- program_files/district_heating.py
+++ program_files/district_heating.py
@@ -68,7 +68,9 @@ def calc_perpendicular_distance_line_point(p1, p2, p3, converted=False):
         
         .. math::
             dx = 111.3 * cos(lat) * (lon1 - lon2)\\
+            
             lat = (lat1 + lat2) / 2 * 0.01745\\
+            
             dy = 111.3 * (lat1 - lat2)
             
         lat1, lat2, lon1, lon2: northern latitude, eastern longitude in
