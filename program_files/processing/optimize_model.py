278cab5ba0b6b43982da6df5aee5a0b6db6ee503
Gregor Becker <63057113+GregorBecker@users.noreply.github.com>
2022-05-17 08:37:41 +0200

    Add comments

--- program_files/processing/optimize_model.py
+++ program_files/processing/optimize_model.py
@@ -22,72 +22,89 @@ def constraint_optimization_against_two_values(om: solph.Model,
     import pyomo.environ as po
     from oemof.solph.plumbing import sequence
     invest_flows = {}
+    ##########################
+    # PERIODICAL CONSTRAINTS #
+    ##########################
+    # extract all investment flows where periodical constraints apply
     for (i, o) in om.flows:
         if hasattr(om.flows[i, o].investment, "periodical_constraint_costs"):
             invest_flows[(i, o)] = om.flows[i, o].investment
-
-    limit_name = "invest_limit_" + "space"
-
+    limit_name = "invest_limit_" + "periodical_constraints"
+    # Setting the equation representing the sum of the periodic
+    # emissions
     setattr(om, limit_name, po.Expression(
         expr=sum(om.InvestmentFlow.invest[inflow, outflow] *
                  getattr(invest_flows[inflow, outflow],
                          "periodical_constraint_costs")
                  for (inflow, outflow) in invest_flows
                  )))
+    ##########################
+    # NONCONVEX CONSTRAINTS  #
+    ##########################
     invest_flows2 = {}
+    # extract all investment flows where fix constraints apply
     for (i, o) in om.flows:
         if hasattr(om.flows[i, o].investment, "fix_constraint_costs"):
             invest_flows2[(i, o)] = om.flows[i, o].investment
-    print(invest_flows2)
-
-    limit_name4 = "invest_limit_" + "nonconvex"
-
-    setattr(om, limit_name4, po.Expression(
+    limit_name1 = "invest_limit_" + "nonconvex_constraints"
+    # Setting the equation representing the sum of the nonconvex
+    # emissions
+    setattr(om, limit_name1, po.Expression(
         expr=sum(getattr(invest_flows2[inflow, outflow],
                          "fix_constraint_costs")
                  for (inflow, outflow) in invest_flows2
                  )))
-
-    ############
+    ##########################
+    # VARIABLE CONSTRAINTS   #
+    ##########################
     flows = {}
+    # extract all investment flows where variable constraints apply
     for (i, o) in om.flows:
         if hasattr(om.flows[i, o], "emission_factor"):
             flows[(i, o)] = om.flows[i, o]
-
-    limit_name1 = "integral_limit_" + "emission_factor"
-
-    setattr(om, limit_name1, po.Expression(
+    limit_name2 = "integral_limit_" + "variable_constraints"
+    # Setting the equation representing the sum of the variable
+    # emissions
+    setattr(om, limit_name2, po.Expression(
         expr=sum(om.flow[inflow, outflow, t]
                  * om.timeincrement[t]
                  * sequence(getattr(flows[inflow, outflow],
                                     "emission_factor"))[t]
                  for (inflow, outflow) in flows
                  for t in om.TIMESTEPS)))
-    
-    ############
+    ##########################
+    # STORAGE CONSTRAINTS    #
+    ##########################
     comp = {}
     comp_fix = {}
+    # extract all investment flows where periodical / fix constraints
+    # apply
     for num in om.GenericInvestmentStorageBlock.INVESTSTORAGES.data():
         if hasattr(num.investment, "periodical_constraint_costs"):
             comp[num] = num.investment
         if hasattr(num.investment, "fix_constraint_costs"):
             comp_fix[num] = num.investment
-    limit_name2 = "invest_limit_storage"
-    setattr(om, limit_name2, po.Expression(
+    # Setting the equation representing the sum of the periodic
+    # emissions
+    limit_name3 = "invest_limit_storage"
+    setattr(om, limit_name3, po.Expression(
             expr=sum(om.GenericInvestmentStorageBlock.invest[num]
                      * getattr(comp[num], "periodical_constraint_costs")
                      for num in comp)))
-    limit_name3 = "invest_limit_fix_storage"
-    setattr(om, limit_name3, po.Expression(
-        expr=sum(getattr(comp[num], "fix_constraint_costs")
-                 for num in comp)))
-
+    # Setting the equation representing the sum of the nonconvex
+    # emissions
+    limit_name4 = "invest_limit_fix_storage"
+    setattr(om, limit_name4, po.Expression(
+        expr=sum(getattr(comp_fix[num], "fix_constraint_costs")
+                 for num in comp_fix)))
+    # Setting the equation representing the overall limit for the sum of
+    # all appearing constraints
     setattr(om, limit_name + "_constraint", po.Constraint(
             expr=((getattr(om, limit_name) + getattr(om, limit_name1)
                    + getattr(om, limit_name2) + getattr(om, limit_name4)
                    + getattr(om, limit_name3))
                   <= limit)))
-
+    # Return of the opimization model extended by the new equations
     return om
 
 
