a8e734e8f2d101c23dd76aa37da1b8843fdb7fa7
Becker <gb611137@fh-muenster.de>
2022-06-15 13:21:27 +0200

    Update create_graph.py

--- program_files/preprocessing/create_graph.py
+++ program_files/preprocessing/create_graph.py
@@ -93,17 +93,19 @@ class ESGraphRenderer:
                 self.add_comp(str(nd.label), "ellipse", False, self.dot)
                 # keep the bus reference for drawing edges later
                 self.busses.append(nd)
-            elif isinstance(nd, Bus) and "infrastructure" in nd.label:
+            else:
                 pass
-            elif isinstance(nd, HeatPipeline):
+            
+            if isinstance(nd, HeatPipeline):
                 if "dh-network" not in str(self.dot):
                     self.dot.node("dh-network", "dh-network", shape="hexagon")
-            else:
+            elif not isinstance(nd, Bus):
                 self.add_comp(
                         str(nd.label),
                         switch_dict.get(str(type(nd)), "Invalid component")[0],
                         switch_dict.get(str(type(nd)), "Invalid component")[1],
                         self.dot)
+                
         # draw the edges between the nodes based on each bus inputs/outputs
         for bus in self.busses:
             for component in bus.inputs:
