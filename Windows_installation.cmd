98d238a66d6bfbde5960a85e8518272cdae81a42
Gregor Becker <63057113+GregorBecker@users.noreply.github.com>
2022-08-05 17:36:31 +0200

    Update Windows_installation.cmd

--- Windows_installation.cmd
+++ Windows_installation.cmd
@@ -11,38 +11,34 @@ start /b activate.bat
 @echo download and install required python packages
 @echo #############################################
 
-pip install pandas==0.25.3
-pip install numpy==1.17.4
-pip install tables==3.5.2
-pip install openpyxl==3.0.0
+pip install "oemof.solph>=0.4"
+pip install "openpyxl>=3.0.0"
+pip install "oemof.thermal>=0.0.5"
 
-pip install oemof.thermal==0.0.3
-pip install demandlib==0.1.6
-pip install pvlib==0.7.1
-echo pip install feedinlib==0.0.12
-pip install oedialect==0.0.10
-pip install sqlalchemy==1.3.16
-pip install open_fred-cli
-pip install richardsonpy==0.2.1
-pip install dash==1.7.0
-pip install dash_canvas==0.1.0
-pip install pydot==1.4.1
-pip install graphviz==0.13.2
-pip install xlrd==1.2.0
-pip install Pyomo==5.7.1
-pip install basemap
-pip install xlsxwriter
-pip install dhnx
-pip install pyproj
-pip install sympy
-pip install numpy==1.17.4
-pip install memory-profiler
-pip install scikit-learn-extra
-pip install seaborn
-pip install matplotlib
-pip install oemof.solph==0.4.1
-pip install numpy==1.17.4
-pip install werkzeug==2.0.3
+echo CHANGE THIS LINE AFTER PR IS MERGED
+pip install git+https://github.com/oemof/feedinlib.git@refs/pull/73/head
+pip install "open_fred-cli>=0.0.1"
+pip install "basemap>=1.3.0"
+
+echo CHANGE THIS LINE AFTER PR IS MERGED
+pip install git+https://github.com/oemof/demandlib.git@refs/pull/51/head
+pip install "richardsonpy>=0.2.0"
+
+pip install "graphviz>=0.20"
+
+pip install "scikit-learn-extra>=0.2.0"
+pip install "memory-profiler>=0.60.0"
+
+pip install "dhnx>=0.0.2"
+pip install "sympy>=1.10.0"
+pip install "osmnx>=1.2.0"
+
+pip install "xlsxwriter>=3.0.0"
+
+pip install "seaborn>=0.11.0"
+
+pip install "dash>=2.4.0"
+pip install "dash_canvas>=0.1.0"
 
 @echo ######################
 @echo Installation completed
