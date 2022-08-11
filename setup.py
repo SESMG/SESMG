8e976fc379fca5e9b4ba5eac0d68dd9842c58fcf
Gregor Becker <63057113+GregorBecker@users.noreply.github.com>
2022-08-11 22:17:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,8 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import print_function
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
 
-import io
-import re
-from glob import glob
-from os.path import basename
-from os.path import dirname
-from os.path import join
-from os.path import splitext
-
-from setuptools import setup, find_packages
-from setuptools.command.install import install as InstallCommand
-
-
-class Install(InstallCommand):
-    """ Customized setuptools install command which uses pip. """
-
-    def run(self, *args, **kwargs):
-        import pip
-        pip.main(['install', '.'])
-        InstallCommand.run(self, *args, **kwargs)
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
 
 setup(
     name="SESMG",
@@ -29,34 +10,15 @@ setup(
     license="MIT",
     author="Christian Klemm",
     author_email="christian.klemm@fh-muenster.de",
-    cmdclass={'install': Install,},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
     classifiers=[
         # complete classifier list:
         # http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9'],
-    install_requires=[
-        "oemof.solph>=0.4",
-        "openpyxl>=3.0.0",
-        "oemof.thermal>=0.0.5",
-        "open_fred-cli>=0.0.1",
-        "basemap>=1.3.0",
-        "richardsonpy>=0.2.0",
-        "graphviz>=0.20",
-        "scikit-learn-extra>=0.2.0",
-        "memory-profiler>=0.60.0",
-        "dhnx>=0.0.2",
-        "sympy>=1.10.0",
-        "osmnx>=1.2.0",
-        "xlsxwriter>=3.0.0",
-        "seaborn>=0.11.0",
-        "dash>=2.4.0",
-        "dash_canvas>=0.1.0"
-        ],
-    dependency_links = [
-     "git+https://github.com/oemof/feedinlib.git@refs/pull/73/head",
-     "git+https://github.com/oemof/demandlib.git@refs/pull/51/head"
-    ],
     extras_require={
         "dev":
             ["pytest", "sphinx", "sphinx_rtd_theme"]
