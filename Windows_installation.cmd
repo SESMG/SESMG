f7a7f572f6a1fb53b2f49d0a54bae6b8a7c69c08
Gregor Becker <63057113+GregorBecker@users.noreply.github.com>
2022-08-12 09:33:14 +0200

    Update Windows_installation.cmd

--- Windows_installation.cmd
+++ Windows_installation.cmd
@@ -3,7 +3,7 @@
 @echo File path: %~dp0
 @echo ################################
 
-py -3.7 -m venv .
+py -3.9 -m venv .
 cd Scripts/
 start /b activate.bat
 
@@ -11,34 +11,7 @@ start /b activate.bat
 @echo download and install required python packages
 @echo #############################################
 
-pip install "oemof.solph>=0.4"
-pip install "openpyxl>=3.0.0"
-pip install "oemof.thermal>=0.0.5"
-
-echo CHANGE THIS LINE AFTER PR IS MERGED
-pip install git+https://github.com/oemof/feedinlib.git@refs/pull/73/head
-pip install "open_fred-cli>=0.0.1"
-pip install "basemap>=1.3.0"
-
-echo CHANGE THIS LINE AFTER PR IS MERGED
-pip install git+https://github.com/oemof/demandlib.git@refs/pull/51/head
-pip install "richardsonpy>=0.2.0"
-
-pip install "graphviz>=0.20"
-
-pip install "scikit-learn-extra>=0.2.0"
-pip install "memory-profiler>=0.60.0"
-
-pip install "dhnx>=0.0.2"
-pip install "sympy>=1.10.0"
-pip install "osmnx>=1.2.0"
-
-pip install "xlsxwriter>=3.0.0"
-
-pip install "seaborn>=0.11.0"
-
-pip install "dash>=2.4.0"
-pip install "dash_canvas>=0.1.0"
+pip install -r requirements.txt
 
 @echo ######################
 @echo Installation completed
