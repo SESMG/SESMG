22051015dfa6e4785f0c0a38efe5feeb222b8dc1
Gregor Becker <63057113+GregorBecker@users.noreply.github.com>
2022-09-26 15:47:58 +0200

    Update Windows_installation.cmd

--- Windows_installation.cmd
+++ Windows_installation.cmd
@@ -21,6 +21,13 @@ pipwin install fiona
 
 pip install -r ..\requirements.txt
 
+mkdir ..\files_deviant_operating_system
+move ..\MacOS_installation.command ..\files_deviant_operating_system\MacOS_installation.command
+move ..\Run_SESMG_for_macos.command ..\files_deviant_operating_system\Run_SESMG_for_macos.command
+move ..\Linux_installation.sh ..\files_deviant_operating_system\Linux_installation.sh
+move ..\Run_SESMG_for_Linux.sh ..\files_deviant_operating_system\Run_SESMG_for_Linux.sh
+
+
 @echo ######################
 @echo Installation completed
 @echo Starting SESMG
