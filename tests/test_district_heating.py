d4c8d5241d44ff450a2e7aa7eccfe3fbac86af3f
Becker <gb611137@fh-muenster.de>
2022-10-04 13:39:04 +0200

    tests(test_district_heating.py): add new unit test

--- tests/test_district_heating.py
+++ tests/test_district_heating.py
@@ -13,3 +13,15 @@ def test_perpendicular_foot_print_calc():
     assert 1 == pytest.approx(test_list[1], 1e-3)
     assert 55600 == pytest.approx(test_list[2], 1e-1)
     assert 0.5 == pytest.approx(test_list[3], 1e-3)
+    
+
+def test_create_fork():
+    from program_files.preprocessing.components.district_heating \
+        import create_fork
+    import dhnx
+    thermal_net = dhnx.network.ThermalNetwork()
+    create_fork(["x", 10, 10, "x", 0.5, "test"], 1, "testbus", thermal_net)
+    assert 10 == int(thermal_net.components["forks"]["lat"])
+    assert 10 == int(thermal_net.components["forks"]["lon"])
+    assert 0.5 == float(thermal_net.components["forks"]["t"])
+    
