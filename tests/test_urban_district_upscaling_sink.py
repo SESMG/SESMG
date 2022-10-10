32de68f76162eea4dcd498d5a25c7572bdcddc9e
Becker <gb611137@fh-muenster.de>
2022-10-10 14:25:23 +0200

    tests(urban_district_upscaling): remove fix values

--- tests/test_urban_district_upscaling_sink.py
+++ tests/test_urban_district_upscaling_sink.py
@@ -56,6 +56,9 @@ def test_create_sinks():
     # import standard parameter
     standard_parameters = pandas.ExcelFile(
         r"program_files/urban_district_upscaling/standard_parameters.xlsx")
+    elec = standard_parameters.parse("ElecDemand")
+    heat = standard_parameters.parse("HeatDemand")
+    sinks = standard_parameters.parse("sinks")
     sheets = {"sinks": pandas.DataFrame()}
     building = {
         "label": "test",
@@ -76,12 +79,16 @@ def test_create_sinks():
     sink = sheets["sinks"].loc[
                 sheets["sinks"]["label"] == "test_electricity_demand"]
     # the demand of a single family house with 5 persons is 5000 kWh/a
-    assert float(sink["annual demand"]) == 5000
+    assert float(sink["annual demand"][0]) == elec.loc[
+        elec["household size"] == "specific demand"]["SFB 5 person"][1]
     # check annual demand column
     sink = sheets["sinks"].loc[
         sheets["sinks"]["label"] == "test_heat_demand"]
     # the demand of a single family house build in 2000 is 131 kWh / (sqm * a)
-    assert float(sink["annual demand"]) == 131 * 0.9 * 100
+    assert float(sink["annual demand"][0]) == float(heat.loc[
+        heat["year of construction"] == 2000]["1 unit(s)"]) \
+        * float(sinks.loc[sinks["sink_type"] == "SFB_heat_sink"][
+                "net_floor_area / area"]) * 100
 
     building = {
         "label": "test1",
@@ -129,12 +136,18 @@ def test_create_sinks():
     sink = sheets["sinks"].loc[
         sheets["sinks"]["label"] == "test2_electricity_demand"]
     # the given electricity demand is 1000 kWh / (occupant * a)
-    assert float(sink["annual demand"]) == 1000 * 6 * 1
+    # * 6 occupants per unit * 1 unit
+    assert float(sink["annual demand"]) == elec.loc[
+        elec["household size"] == "specific demand"]["SFB 5 person"][1] / 5 \
+        * 6 * 1
     # check annual demand column
     sink = sheets["sinks"].loc[
         sheets["sinks"]["label"] == "test2_heat_demand"]
     # the given electricity demand is 100 kWh / (sqm * a)
-    assert float(sink["annual demand"]) == 175 * 100 * 0.9
+    assert float(sink["annual demand"]) == float(heat.loc[
+        heat["year of construction"] == 1980]["1 unit(s)"]) * 100 \
+        * float(sinks.loc[sinks["sink_type"] == "SFB_heat_sink"][
+                "net_floor_area / area"])
 
     building = {
         "label": "test3",
@@ -156,12 +169,45 @@ def test_create_sinks():
     sink = sheets["sinks"].loc[
         sheets["sinks"]["label"] == "test3_electricity_demand"]
     # the given electricity demand is 1000 kWh / (occupant * a)
-    assert float(sink["annual demand"]) == 17 * 100 * 0.85
+    assert float(sink["annual demand"]) == elec.loc[
+        elec["household size"] == "specific demand"][
+        "COM_Office"][1] * 100 \
+        * float(sinks.loc[sinks["sink_type"] == "COM_Office_electricity_sink"][
+                "net_floor_area / area"])
     # check annual demand column
     sink = sheets["sinks"].loc[
         sheets["sinks"]["label"] == "test3_heat_demand"]
     # the given electricity demand is 100 kWh / (sqm * a)
-    assert float(sink["annual demand"]) == 136.3 * 100 * 0.85
+    assert float(sink["annual demand"]) == 136.3 * 100 \
+        * float(sinks.loc[sinks["sink_type"] == "COM_Office_heat_sink"][
+                "net_floor_area / area"])
+
+    building = {
+        "label": "test4",
+        "building type": "COM_Office",
+        "gross building area": 100,
+        "occupants per unit": 0,
+        "units": 1,
+        "electricity demand": 1000,
+        "year of construction": 0,
+        "heat demand": 1000
+    }
+
+    # create a standard_parameter building res electricity bus
+    sheets = Sink.create_sinks(
+            building=building,
+            sheets=sheets,
+            standard_parameters=standard_parameters)
+    # check annual demand column
+    sink = sheets["sinks"].loc[
+        sheets["sinks"]["label"] == "test4_electricity_demand"]
+    # the given electricity demand is 1000 kWh / (occupant * a)
+    assert float(sink["annual demand"]) == 1000 * 100
+    # check annual demand column
+    sink = sheets["sinks"].loc[
+        sheets["sinks"]["label"] == "test4_heat_demand"]
+    # the given electricity demand is 100 kWh / (sqm * a)
+    assert float(sink["annual demand"]) == 1000 * 100
     
 
 def test_sink_clustering():
