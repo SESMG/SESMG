a3749136f4060fa8d5c4001daa73514feea5e00e
Becker <gb611137@fh-muenster.de>
2022-10-10 14:42:47 +0200

    documentation(urban_district_upscaling): sink test

--- tests/test_urban_district_upscaling_sink.py
+++ tests/test_urban_district_upscaling_sink.py
@@ -56,10 +56,17 @@ def test_create_sinks():
     # import standard parameter
     standard_parameters = pandas.ExcelFile(
         r"program_files/urban_district_upscaling/standard_parameters.xlsx")
+    # import elec demand table
     elec = standard_parameters.parse("ElecDemand")
+    # import heat demand table
     heat = standard_parameters.parse("HeatDemand")
+    # import sinks table
     sinks = standard_parameters.parse("sinks")
     sheets = {"sinks": pandas.DataFrame()}
+    
+    # prepare FIRST TEST BUILDING:
+    # details: Single family building, 100 sqm building area, yoc 2000,
+    # 5 person
     building = {
         "label": "test",
         "building type": "SFB",
@@ -70,26 +77,29 @@ def test_create_sinks():
         "year of construction": 2000,
         "heat demand": 0
     }
-    # create a standard_parameter building res electricity bus
     sheets = Sink.create_sinks(
             building=building,
             sheets=sheets,
             standard_parameters=standard_parameters)
-    # check annual demand column
+    
+    # check annual ELEC DEMAND column
     sink = sheets["sinks"].loc[
                 sheets["sinks"]["label"] == "test_electricity_demand"]
-    # the demand of a single family house with 5 persons is 5000 kWh/a
+    # the demand of a single family house with 5 persons
     assert float(sink["annual demand"][0]) == elec.loc[
         elec["household size"] == "specific demand"]["SFB 5 person"][1]
-    # check annual demand column
+    
+    # check annual HEAT DEMAND column
     sink = sheets["sinks"].loc[
         sheets["sinks"]["label"] == "test_heat_demand"]
-    # the demand of a single family house build in 2000 is 131 kWh / (sqm * a)
+    # the demand of a single family house build in 2000
     assert float(sink["annual demand"][0]) == float(heat.loc[
         heat["year of construction"] == 2000]["1 unit(s)"]) \
         * float(sinks.loc[sinks["sink_type"] == "SFB_heat_sink"][
                 "net_floor_area / area"]) * 100
-
+    
+    # prepare SECOND TEST BUILDING:
+    # details: Single family building, energy certificate information
     building = {
         "label": "test1",
         "building type": "SFB",
@@ -100,22 +110,25 @@ def test_create_sinks():
         "year of construction": 0,
         "heat demand": 100
     }
-    # create a standard_parameter building res electricity bus
     sheets = Sink.create_sinks(
             building=building,
             sheets=sheets,
             standard_parameters=standard_parameters)
-    # check annual demand column
+    
+    # check annual ELEC DEMAND column
     sink = sheets["sinks"].loc[
         sheets["sinks"]["label"] == "test1_electricity_demand"]
     # the given electricity demand is 100 kWh / (sqm * a)
     assert float(sink["annual demand"]) == 100 * 100
-    # check annual demand column
+    
+    # check annual HEAT DEMAND column
     sink = sheets["sinks"].loc[
         sheets["sinks"]["label"] == "test1_heat_demand"]
     # the given electricity demand is 100 kWh / (sqm * a)
     assert float(sink["annual demand"]) == 100 * 100
-
+    
+    # prepare THIRD TEST BUILDING:
+    # details: Single family building, >5 person, yoc 1980
     building = {
         "label": "test2",
         "building type": "SFB",
@@ -126,62 +139,68 @@ def test_create_sinks():
         "year of construction": 1980,
         "heat demand": 0
     }
-    
-    # create a standard_parameter building res electricity bus
     sheets = Sink.create_sinks(
             building=building,
             sheets=sheets,
             standard_parameters=standard_parameters)
-    # check annual demand column
+    
+    # check annual ELEC DEMAND column
     sink = sheets["sinks"].loc[
         sheets["sinks"]["label"] == "test2_electricity_demand"]
-    # the given electricity demand is 1000 kWh / (occupant * a)
-    # * 6 occupants per unit * 1 unit
+    # the given electricity demand is 1000 kWh / (occupant * a) (based
+    # on the current value within the standard_parameters 5000 kWh / a
+    # / 5 person) * 6 occupants per unit * 1 unit
     assert float(sink["annual demand"]) == elec.loc[
         elec["household size"] == "specific demand"]["SFB 5 person"][1] / 5 \
         * 6 * 1
-    # check annual demand column
+
+    # check annual HEAT DEMAND column
     sink = sheets["sinks"].loc[
         sheets["sinks"]["label"] == "test2_heat_demand"]
-    # the given electricity demand is 100 kWh / (sqm * a)
+    # the given heat demand for the specific year of construction
     assert float(sink["annual demand"]) == float(heat.loc[
         heat["year of construction"] == 1980]["1 unit(s)"]) * 100 \
         * float(sinks.loc[sinks["sink_type"] == "SFB_heat_sink"][
                 "net_floor_area / area"])
-
+    
+    # prepare Forth TEST BUILDING:
+    # details: Commercial building, yoc 1980
     building = {
         "label": "test3",
         "building type": "COM_Office",
         "gross building area": 100,
-        "occupants per unit": 6,
+        "occupants per unit": 0,
         "units": 1,
         "electricity demand": 0,
         "year of construction": 1980,
         "heat demand": 0
     }
-    
-    # create a standard_parameter building res electricity bus
     sheets = Sink.create_sinks(
             building=building,
             sheets=sheets,
             standard_parameters=standard_parameters)
-    # check annual demand column
+    
+    # check annual ELEC DEMAND column
     sink = sheets["sinks"].loc[
         sheets["sinks"]["label"] == "test3_electricity_demand"]
-    # the given electricity demand is 1000 kWh / (occupant * a)
+    # the given electricity demand
     assert float(sink["annual demand"]) == elec.loc[
         elec["household size"] == "specific demand"][
         "COM_Office"][1] * 100 \
         * float(sinks.loc[sinks["sink_type"] == "COM_Office_electricity_sink"][
                 "net_floor_area / area"])
-    # check annual demand column
+    
+    # check annual HEAT DEMAND column
     sink = sheets["sinks"].loc[
         sheets["sinks"]["label"] == "test3_heat_demand"]
     # the given electricity demand is 100 kWh / (sqm * a)
-    assert float(sink["annual demand"]) == 136.3 * 100 \
+    assert float(sink["annual demand"]) == float(heat.loc[
+        heat["year of construction"] == 1980]["COM_Office"]) * 100 \
         * float(sinks.loc[sinks["sink_type"] == "COM_Office_heat_sink"][
                 "net_floor_area / area"])
 
+    # prepare Fifth TEST BUILDING:
+    # details: Commercial building, energy certificate information
     building = {
         "label": "test4",
         "building type": "COM_Office",
@@ -192,18 +211,18 @@ def test_create_sinks():
         "year of construction": 0,
         "heat demand": 1000
     }
-
-    # create a standard_parameter building res electricity bus
     sheets = Sink.create_sinks(
             building=building,
             sheets=sheets,
             standard_parameters=standard_parameters)
-    # check annual demand column
+    
+    # check annual ELEC DEMAND column
     sink = sheets["sinks"].loc[
         sheets["sinks"]["label"] == "test4_electricity_demand"]
     # the given electricity demand is 1000 kWh / (occupant * a)
     assert float(sink["annual demand"]) == 1000 * 100
-    # check annual demand column
+    
+    # check annual HEAT DEMAND column
     sink = sheets["sinks"].loc[
         sheets["sinks"]["label"] == "test4_heat_demand"]
     # the given electricity demand is 100 kWh / (sqm * a)
