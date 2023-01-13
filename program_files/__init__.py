9134ebe51ae1f9c0657ccb1bc61b87e4e7ce175c
Becker <gb611137@fh-muenster.de>
2023-01-13 14:12:58 +0100

    code( ): resolve merge conflicts

--- program_files/__init__.py
+++ program_files/__init__.py
@@ -1,1 +1,10 @@
-__version__ = "0.4.2"
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 09:35:53 2020
+
+@author: Christian
+"""
+from .urban_district_upscaling.pre_processing import *
+from .urban_district_upscaling.components import *
+from .preprocessing.components import *
+from .preprocessing import *
--- program_files/postprocessing/Interactive_Results.py
+++ program_files/postprocessing/Interactive_Results.py
@@ -15,10 +15,7 @@ The HTML-Page consists the following elements:
     
 """
 
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_table
+from dash import Dash, html, dash_table, dcc
 import pandas as pd
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
@@ -34,7 +31,7 @@ if sys.argv[1] != "-T":
         result_path_import = result_path_import[1:]
 # necessary for Sphinx documentation
 else:
-    result_path_import = "../../results"
+    result_path_import = "../results"
 
 
 def return_component_value(componentid, table):
@@ -95,7 +92,7 @@ external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 options = dict()
 
 # Creating the application, which is running the interactive page
-demo_app = dash.Dash(__name__, **options)
+demo_app = Dash(__name__, **options)
 
 demo_app.layout = html.Div(
     children=[
