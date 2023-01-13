6bf07da50faabf728d7bd996686faefca3f14de3
Becker <gb611137@fh-muenster.de>
2023-01-13 15:13:49 +0100

    code(start_script.py): resolve merge conflicts

--- start_script.py
+++ start_script.py
@@ -1,8 +1,25 @@
 import sys
 import subprocess
+import os
 
 if __name__ == "__main__":
+    #if "-c" in sys.argv:
+    #    import program_files.GUI_files.cluster_GUI as cluster_gui
+    #else:
+    #    if "-s" in sys.argv:
     if sys.platform.startswith("darwin"):
-        import program_files.GUI
+        #import program_files.GUI_files.GUI as GUI
+
+        #gui = GUI.GUI()
+        subprocess.call("streamlit run {}".format(os.path.dirname(__file__) + "/GUI_streamlit.py"), shell=True)
+
+
+    elif sys.platform.startswith("linux"):
+        import program_files.GUI_files.GUI as GUI
+
+        gui = GUI.GUI()
     else:
-        subprocess.call("Scripts/python program_files/GUI.py")
+        import program_files.GUI_files.GUI as GUI
+
+        gui = GUI.GUI()
+        # subprocess.call("Scripts/python program_files/GUI_files/GUI.py")
