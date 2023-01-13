a661a13e83b3b2dab7c7aa66831bf2fc80bee114
Becker <gb611137@fh-muenster.de>
2023-01-13 15:40:01 +0100

    code(create_graph): resolve merge conflicts

--- program_files/preprocessing/create_graph.py
+++ program_files/preprocessing/create_graph.py
@@ -1,228 +1,156 @@
-from graphviz import Digraph
+# -*- coding: utf-8 -*-
+
+"""Module to render an oemof energy model network in a graph.
+SPDX-FileCopyrightText: Pierre-Francois Duc <pierre-francois.duc@rl-institut.de>
+SPDX-FileCopyrightText: Uwe Krien <krien@uni-bremen.de>
+SPDX-FileCopyrightText: Patrik Schönfeldt
+SPDX-License-Identifier: MIT
+"""
+
+import logging
 import os
+import graphviz
+from oemof.network.network import Bus, Sink, Source, Transformer
+from oemof.solph.components import GenericStorage
+from dhnx.optimization.oemof_heatpipe import HeatPipeline
 
 
-def create_graph(filepath: str, nodes_data: dict, show: bool, legend=False):
+def linebreaks(text: str):
     """
-    Visualizes the energy system as graph.
+    Adds linebreaks a given string.
 
-    Creates, using the library Graphviz, a graph containing all
-    components and connections from "nodes_data" and returns this as
-    a PNG file.
+    Function which adds a line break to strings every ten
+    characters. Up to four strings are added.
 
-    :param filepath: path where the PNG-result shall be saved
-    :type filepath: str
-    :param nodes_data: dictionary containing data from excel
-                       scenario file.
-    :type nodes_data: dict
-    :param legend: specifies, whether a legend will be added to the
-                   graph or not
-    :type legend: bool
+    :param text: string to which line breaks will be added
+    :type text: str
 
     Christian Klemm - christian.klemm@fh-muenster.de
     """
+    text_length = len(text)
+    if text_length > 10:
+        text = str(text[0:9] + "-\n" + text[9:])
+    if text_length > 20:
+        text = str(text[0:21] + "-\n" + text[21:])
+    if text_length > 30:
+        text = str(text[0:33] + "-\n" + text[33:])
+    if text_length > 40:
+        text = str(text[0:45] + "-\n" + text[45:])
+    return text
 
-    def linebreaks(text: str):
+
+class ESGraphRenderer:
+    def __init__(
+        self, energy_system=None, filepath=None, legend=False, view=False
+    ) -> None:
         """
-        Adds linebreaks a given string.
-
-        Function which adds a line break to strings every ten
-        characters. Up to four strings are added.
-
-        :param text: string to which line breaks will be added
-        :type text: str
-
-        Christian Klemm - christian.klemm@fh-muenster.de
+            Render an oemof energy system using graphviz.
+            
+            developed by the oemof group adopted for SESMG
+            original repository: https://github.com/oemof/oemof-visio
+        
+            :param energy_system: The oemof energy system
+            :type energy_system: oemof.solph.network.EnergySystem
+            :param filepath: path where the created files will be saved
+            :type filepath: str
+            :param legend: specify, whether a legend will be added to \
+                the graph or not
+            :type legend: bool
         """
-        text_length = len(text)
-        if text_length > 10:
-            text = str(text[0:9] + "-\n" + text[9:])
-        if text_length > 20:
-            text = str(text[0:21] + "-\n" + text[21:])
-        if text_length > 30:
-            text = str(text[0:33] + "-\n" + text[33:])
-        if text_length > 40:
-            text = str(text[0:45] + "-\n" + text[45:])
-        return text
+        self.busses = []
+        os.environ["PATH"] += os.pathsep + "C:\\Program Files (x86)\\Graphviz2.38\\bin"
 
-    # Defines the location of Graphviz as path necessary for windows
-    os.environ["PATH"] += os.pathsep + "C:\\Program Files (x86)\\Graphviz2.38\\bin"
-    # Creates the Directed-Graph
-    dot = Digraph(format="png")
-    # Creates a Legend if Legend = True
-    if legend:
-        shape = {
-            "Bus": ["ellipse"],
-            "Source": ["trapezium"],
-            "Sink": ["invtrapezium"],
-            "Transformer\nLinks": ["box"],
-            "Storage": ["box"],
+        self.dot = graphviz.Digraph(format="png")
+
+        if legend is True:
+            with self.dot.subgraph(name="cluster_1") as self.c:
+                # color of the legend box
+                self.c.attr(color="black")
+                # title of the legend box
+                self.c.attr(label="Legends")
+                self.add_comp("Bus", "ellipse", False, self.c)
+                self.add_comp("Sink", "invtrapezium", False, self.c)
+                self.add_comp("Source", "trapezium", False, self.c)
+                self.add_comp("Transformer", "rectangle", False, self.c)
+                self.add_comp("Storage", "rectangle", True, self.c)
+
+        switch_dict = {
+            "<class 'oemof.solph.network.sink.Sink'>": ["invtrapezium", False],
+            "<class 'oemof.solph.network.source.Source'>": ["trapezium", False],
+            "<class 'oemof.solph.network.transformer.Transformer'>": [
+                "rectangle",
+                False,
+            ],
+            "<class 'oemof.solph.components.generic_storage.GenericStorage'>": [
+                "rectangle",
+                True,
+            ],
+            "<class 'oemof.solph.custom.link.Link'>": ["rectangle", False],
         }
-        for i in shape.keys():
-            dot.node(
-                i,
-                shape=shape[i][0],
-                fontsize="10",
-                fixedsize="shape",
-                width="1.1",
-                height="0.6",
-                style="dashed" if i == "Storage" else "",
-            )
-    shapes = {
-        "sources": ["trapezium"],
-        "sinks": ["invtrapezium"],
-        "transformers": ["box"],
-        "storages": ["box"],
-        "links": ["box"],
-    }
-    bus = {
-        "buses": ["label"],
-        "sources": ["output"],
-        "sinks": ["input"],
-        "transformers": ["input"],
-        "storages": ["bus"],
-        "links": ["bus1"],
-    }
-    for i in bus.keys():
-        for j, b in nodes_data[i].iterrows():
-            if b["active"]:
-                # sets component label
-                label = b["label"]
-                if i == "buses":
-                    if b["shortage"]:
-                        label = b["label"] + "_shortage"
-                    elif b["excess"]:
-                        label = b["label"] + "_excess"
-                label = linebreaks(label)
-                if i != "buses":
-                    dot.node(
-                        label,
-                        shape=shapes[i][0],
-                        fontsize="10",
-                        fixedsize="shape",
-                        width="1.1",
-                        height="0.6",
-                        style="dashed" if i == "storages" else "",
-                    )
-                    if i == "sources":
-                        # Creates graph elements for solar heat
-                        if (
-                            b["technology"] == "solar_thermal_flat_plate"
-                            or b["technology"] == "CSP"
-                        ):
-                            # creates additional transformer
-                            transformer = b["label"] + "_collector"
-                            transformer = linebreaks(transformer)
-                            dot.node(
-                                transformer,
-                                shape="box",
-                                fontsize="10",
-                                fixedsize="shape",
-                                width="1.1",
-                                height="0.6",
-                            )
-                            # creates additional bus
-                            c_bus = b["label"] + "_bus"
-                            c_bus = linebreaks(c_bus)
-                            dot.node(c_bus, shape="ellipse", fontsize="10")
-                            # Adds edge for transformer, source and bus to the graph
-                            dot.edge(b["input"], transformer)
-                            dot.edge(c_bus, transformer)
-                            dot.edge(transformer, b["output"])
-                            dot.edge(label, c_bus)
-                else:
-                    if b["shortage"]:
-                        dot.node(
-                            label,
-                            shape="trapezium",
-                            fontsize="10",
-                            fixedsize="shape",
-                            width="1.1",
-                            height="0.6",
-                        )
+        # draw a node for each of the energy_system's component.
+        # the shape depends on the component's type.
+        for nd in energy_system.nodes:
+            # make sur the label is a string and not a tuple
+            if isinstance(nd, Bus) and not "infrastructure" in nd.label:
+                self.add_comp(str(nd.label), "ellipse", False, self.dot)
+                # keep the bus reference for drawing edges later
+                self.busses.append(nd)
+            elif isinstance(nd, Bus) and "infrastructure" in nd.label:
+                pass
+            elif isinstance(nd, HeatPipeline):
+                if "dh-network" not in str(self.dot):
+                    self.dot.node("dh-network", "dh-network", shape="hexagon")
+            else:
+                self.add_comp(
+                    str(nd.label),
+                    switch_dict.get(str(type(nd)), "Invalid component")[0],
+                    switch_dict.get(str(type(nd)), "Invalid component")[1],
+                    self.dot,
+                )
+        # draw the edges between the nodes based on each bus inputs/outputs
+        for bus in self.busses:
+            for component in bus.inputs:
+                # draw an arrow from the component to the bus
+                self.connect(component, bus)
+            for component in bus.outputs:
+                # draw an arrow from the bus to the component
+                self.connect(bus, component)
 
-                    if b["excess"] and not b["shortage"]:
-                        dot.node(
-                            label,
-                            shape="invtrapezium",
-                            fontsize="10",
-                            fixedsize="shape",
-                            width="1.1",
-                            height="0.6",
-                        )
-                # creates bus nodes
-                dot.node(b[bus[i][0]], shape="ellipse", fontsize="10")
-                if i == "links":
-                    dot.node(b["bus2"], shape="ellipse")
-                # creates edges
-                if (
-                    i == "sinks"
-                    or i == "storages"
-                    or i == "links"
-                    or (i == "buses" and b["excess"] and not b["shortage"])
-                ):
-                    dot.edge(b[bus[i][0]], label)
-                if (
-                    (
-                        i == "sources"
-                        and (b["technology"] not in ["solar_thermal_flat_plate", "CSP"])
-                    )
-                    or i == "storages"
-                    or (i == "buses" and b["shortage"])
-                ):
-                    dot.edge(label, b[bus[i][0]])
-                if i == "links":
-                    dot.edge(label, b["bus2"])
-                    if b["(un)directed"] == "undirected":
-                        dot.edge(b["bus2"], label)
-                        dot.edge(label, b["bus1"])
-                elif i == "transformers":
-                    dot.node(b["output"], shape="ellipse", fontsize="10")
-                    dot.edge(b[bus[i][0]], label)
-                    dot.edge(label, b["output"])
-                    if b["output2"] not in [0, "None", "none"]:
-                        dot.node(b["output2"], shape="ellipse", fontsize="10")
-                        dot.edge(label, b["output2"])
-                    if b["input2"] not in [0, "None", "none"]:
-                        dot.node(b["input2"], shape="ellipse", fontsize="10")
-                        dot.edge(b["input2"], label)
-                    if b["transformer type"] == "compression_heat_transformer":
-                        # consideration of mode of operation
-                        if b["mode"] == "heat_pump":
-                            temp = "_low_temp"
-                        elif b["mode"] == "chiller":
-                            temp = "_high_temp"
-                        # creates label for source and bus depending on mode
-                        cmpr_abs_source = label + temp + "_source"
-                        cmpr_abs_bus = label + temp + "_bus"
-                        # Linebreaks, so that the labels fit the boxes
-                        cmpr_abs_source = linebreaks(cmpr_abs_source)
-                        cmpr_abs_bus = linebreaks(cmpr_abs_bus)
-                        # Adds a second input and a heat source (node and edge)
-                        # for compressionand absorption heat transformers
-                        dot.node(cmpr_abs_bus, shape="ellipse", fontsize="10")
-                        dot.edge(cmpr_abs_bus, label)
-                        dot.node(
-                            cmpr_abs_source,
-                            shape="trapezium",
-                            fontsize="10",
-                            fixedsize="shape",
-                            width="1.1",
-                            height="0.6",
-                        )
-                        dot.edge(cmpr_abs_source, cmpr_abs_bus)
-                elif i == "buses":
-                    if b["excess"] and b["shortage"]:
-                        label = b["label"] + "_excess"
-                        label = linebreaks(label)
-                        dot.node(
-                            label,
-                            shape="invtrapezium",
-                            fontsize="10",
-                            fixedsize="shape",
-                            width="1.1",
-                            height="0.6",
-                        )
-                        dot.node(b[bus[i][0]], shape="ellipse", fontsize="10")
-                        dot.edge(b[bus[i][0]], label)
-    dot.render(filepath + "/graph.gv", view=show)
+        self.render(filepath, view)
+
+    def add_comp(self, label, shape, dashed, subgraph):
+        subgraph.node(
+            linebreaks(label),
+            shape=shape,
+            fontsize="10",
+            fixedsize="shape",
+            width="1.1",
+            height="0.7",
+            style="dashed" if dashed else "",
+        )
+
+    def connect(self, a, b):
+        """Draw an arrow from node a to node b
+
+        Parameters
+        ----------
+        a: `oemof.solph.network.Node`
+            An oemof node (usually a Bus or a Component)
+
+        b: `oemof.solph.network.Node`
+            An oemof node (usually a Bus or a Component)
+        """
+        a_label = linebreaks(str(a.label))
+        b_label = linebreaks(str(b.label))
+        if isinstance(b, HeatPipeline):
+            self.dot.edge(a_label, "dh-network")
+        elif isinstance(a, HeatPipeline):
+            self.dot.edge("dh-network", b_label)
+        elif isinstance(a, HeatPipeline) and isinstance(b, HeatPipeline):
+            pass
+        else:
+            self.dot.edge(a_label, b_label)
+
+    def render(self, filepath, show):
+        """Call the render method of the DiGraph instance"""
+        self.dot.render(filepath + "/graph.gv", view=show)
