5246394de9495348d39fde88978872af5c003de0
Gregor Becker <63057113+GregorBecker@users.noreply.github.com>
2023-01-26 16:58:52 +0100

    code(pareto_curve_plotting.py): remove wrongly pushed lines

--- program_files/postprocessing/pareto_curve_plotting.py
+++ program_files/postprocessing/pareto_curve_plotting.py
@@ -47,42 +47,16 @@ def create_pareto_plot(result_dfs: dict, result_path: str) -> None:
     plt.savefig(result_path + "/pareto.jpeg")
 
 
-#if __name__ == "__main__":
-#    create_pareto_plot(
-#        result_dfs={"1": pd.read_csv(""), "0.5": pd.read_csv(""), "0": pd.read_csv("")},
-#        result_path="",
-#    )
 if __name__ == "__main__":
     from program_files.preprocessing.create_energy_system import import_scenario
     create_pareto_plot(
-            {"1": pd.read_csv(
-                    "/Users/gregor/sciebo/VM102/SESMG_20221111/results/2022"
-                    "-12-01--16-12-39/20221129_SchlossST_model_definition_v2_2022-12-01--16-12-39/components.csv"),
-                "0.75": pd.read_csv(
-                        "/Users/gregor/sciebo/VM102/SESMG_20221111/results"
-                        "/2022-12-01--16-12-39/20221129_SchlossST_model_definition_v2_0.25_2022-12-02--22-41-20/components.csv"),
-                "0.5": pd.read_csv(
-                        "/Users/gregor/sciebo/VM102/SESMG_20221111/results"
-                        "/2022-12-01--16-12-39/20221129_SchlossST_model_definition_v2_0.5_2022-12-01--19-46-12/components.csv"),
-                "0.35": pd.read_csv(
-                        "/Users/gregor/sciebo/VM102/SESMG_20221111/results"
-                        "/2022-12-01--16-12-39/20221129_SchlossST_model_definition_v2_0.65_2022-12-04--20-18-29/components.csv"),
-                "0.25": pd.read_csv(
-                        "/Users/gregor/sciebo/VM102/SESMG_20221111/results"
-                        "/2022-12-01--16-12-39/20221129_SchlossST_model_definition_v2_0.75_2022-12-02--06-05-14/components.csv"),
-                "0.15": pd.read_csv(
-                        "/Users/gregor/sciebo/VM102/SESMG_20221111/results"
-                        "/2022-12-01--16-12-39/20221129_SchlossST_model_definition_v2_0.85_2022-12-04--12-09-51/components.csv"),
-                "0": pd.read_csv(
-                        "/Users/gregor/sciebo/VM102/SESMG_20221111/results"
-                        "/2022-12-01--16-12-39/20221129_SchlossST_model_definition_v2_0_2022-12-01--19-09-05/components.csv")},
-            #import_scenario(
-            #    "/Users/gregor/Downloads/2022-10-24--07-47-24/20221020_SchlossST_variant_1_0.75.xlsx"),
-            
-            # import_scenario(
-            # "/Users/gregor/Desktop/Arbeit/Git/SESMG/results/2022-10-20--17
-            # -52-20/20221018_schlossST_Optimierung2_0.5.xlsx"),
-            # "/Users/gregor/Desktop/Arbeit/Git/SESMG/results/2022-10-20--17
-            # -52-20/"
-            str(os.path.dirname(__file__) + "/v2_final")
+            {"1": pd.read_csv("<path_to_csv_file>"),
+             "0.75": pd.read_csv(),
+             "0.5": pd.read_csv(),
+             "0.35": pd.read_csv(),
+             "0.25": pd.read_csv(),
+             "0.15": pd.read_csv(),
+             "0": pd.read_csv()},
+            # result path 
+            str()
     )
