5cf6079acc1aa78016dde5981e0df040cd1f9c8c
Becker <gb611137@fh-muenster.de>
2023-01-30 10:48:26 +0100

    tests(bus): undo last changes

--- tests/test_preprocessing_bus.py
+++ tests/test_preprocessing_bus.py
@@ -27,7 +27,6 @@ def test_bus_entry():
     return test_energy_system.nodes
 
 
-@pytest.mark.skip
 def test_buses(test_bus_entry):
     """
         Now the Pandas DataFrame structure of the Buses method is
