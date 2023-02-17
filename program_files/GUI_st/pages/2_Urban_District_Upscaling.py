cc9c590ebf15b05cc5726fe6f52b8b6e2e3f2d29
Janik Budde <janikbudde@googlemail.com>
2023-02-17 12:10:40 +0100

    Update 2_Urban_District_Upscaling.py
    not ready, i'll fix it on tuesday

--- program_files/GUI_st/pages/2_Urban_District_Upscaling.py
+++ program_files/GUI_st/pages/2_Urban_District_Upscaling.py
@@ -5,13 +5,9 @@ import os
 from program_files.urban_district_upscaling.pre_processing \
     import urban_district_upscaling_pre_processing
 from GUI_st_global_functions import st_settings_global
-from io import BytesIO
 
-####################################
 # settings the initial streamlit page settings
 st_settings_global()
-
-
 
 
 def us_application():
@@ -33,17 +29,17 @@ def us_application():
         with tab_us:
             # input us sheet
             input_us_sheet_path = st.file_uploader(
-                    "Import your upscaling sheet:")
+                "Import your upscaling sheet:")
 
         with tab_sp:
             # input standard parameter sheet
             input_standard_parameter_path = st.file_uploader(
-                    "Import your standard parameter sheet:")
+                "Import your standard parameter sheet:")
 
         with tab_md:
             result_file_name = \
                 st.text_input("Type in your model definition file name.")
-            
+
         # Run program main function if start button is clicked
         if submitted_us_run:
             if input_us_sheet_path != "" \
@@ -70,34 +66,35 @@ def us_application():
                                data=model_definition_df,
                                file_name=result_file_name + ".xlsx")
     st.session_state["model_definition"] = model_definition_df
-    udu_page = st.container()
 
-    with udu_page:
-        # defining two columns
-        introduction, overview_md = st.columns([2, 1])
-        # first column
-        introduction.markdown(" # Urban District Upscaling Tool")
-        introduction.markdown(" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
-                              " incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud "
-                              "exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
-                              "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu "
-                              "fugiat nulla pariatur. "
-                              "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia "
-                              "deserunt mollit anim id est laborum. ")
 
-        # second column
-        overview_md.markdown(" # Model definition ")
+def standard_page():
+    st.markdown(" # Urban District Upscaling Tool")
+    st.markdown(" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+                " incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud "
+                "exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu "
+                "fugiat nulla pariatur. "
+                "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia "
+                "deserunt mollit anim id est laborum. ")
 
-        if "xlsx" not in st.session_state:
-            st.session_state["xlsx"] = "not done"
-        test = overview_md.file_uploader("Model definition", on_change=change_xlsx_state)
-        if st.session_state["xlsx"] == "done":
-            overview_md.success("xlsx file uploaded successfully")
 
-            with st.expander("Show the sinks of the model definition"):
-                model_definition = pd.ExcelFile(test)
-                df1 = pd.read_excel(model_definition, 'sinks')
-                st.dataframe(df1)
+# second column
+def after_processing_page():
+    st.header("Model defintion")
+
+    if "xlsx" not in st.session_state:
+        st.session_state["xlsx"] = "not done"
+    input_model_definition = st.file_uploader("Model definition", on_change=change_xlsx_state)
+    if st.session_state["xlsx"] == "done":
+        tabs = pd.ExcelFile(input_model_definition).sheet_names
+        # without info column
+        tabs.pop(0)
+        tab_bar = st.tabs(tabs)
+        for i in range(len(tabs)):
+            with tab_bar[i]:
+                df_input = pd.read_excel(input_model_definition, tabs[i])
+                st.dataframe(df_input)
 
 
 def change_xlsx_state():
@@ -108,4 +105,12 @@ def creating_xlsx():
     st.session_state["created"] = "done"
 
 
+def run_active():
+    st.session_state["active_us"] = "done"
+
+
 us_application()
+if st.session_state["model_definition"] == "":
+    standard_page()
+else:
+    after_processing_page()
