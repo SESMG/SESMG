f26311a6601d82195e65e55aa5a6f7399da0052c
jtock <jan.tockloth@fh-muenster.de>
2023-02-25 19:14:24 +0100

    code(demo_tool): implement criterion switch and remove unneccessary files

--- program_files/GUI_st/pages/4_Demo_Tool.py
+++ program_files/GUI_st/pages/4_Demo_Tool.py
@@ -6,25 +6,27 @@
 
 import os
 import openpyxl
+import glob
 import streamlit as st
 import pandas as pd
+from PIL import Image
 from program_files.preprocessing.Spreadsheet_Energy_System_Model_Generator \
     import sesmg_main
 from program_files.GUI_st.GUI_st_global_functions import \
-    st_settings_global
+    st_settings_global, read_markdown_document
 
 
 # creating global model run mode dict
 mode_dict = {
     "monetary": [
-        "demo_model_definition_monetaer.xlsx",
-        r"/results/demo/financial",
+        "demo_model_definition.xlsx",
+        r"/results/demo",
         "Total System Costs",
         "Total Constraint Costs",
     ],
     "emissions": [
-        "demo_model_definition_emissionen.xlsx",
-        r"/results/demo/emissions",
+        "demo_model_definition.xlsx",
+        r"/results/demo",
         "Total Constraint Costs",
         "Total System Costs",
     ],
@@ -32,6 +34,12 @@ mode_dict = {
 
 # creating global input values dict
 input_values_dict = {}
+
+# define main path to SESMG main folder
+mainpath_mf = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.dirname(__file__))))
+# define main path to SESMG program files folder
+mainpath_pf = os.path.join(mainpath_mf, "program_files")
 
 # setting initial session state for mdoel run
 if "state_submitted_demo_run" not in st.session_state:
@@ -49,42 +57,43 @@ def dt_input_sidebar():
     with st.sidebar.form("Simulation input"):
 
         # input value for model run name
-        st.text_input(label="Name")
+        st.text_input(label="Name",
+                      value="Name")
 
         # input value for photovoltaiks
         input_values_dict["input_pv"] = st.number_input(
             label="Photovoltaic in kW",
             min_value=0,
             max_value=10000,
-            step=1000)
+            step=1)
 
         # input value for solar thermal
         input_values_dict["input_st"] = st.number_input(
             label="Solar Thermal in kW",
             min_value=0,
             max_value=27700,
-            step=1000)
+            step=1)
 
         # input value for central thermal storage
         input_values_dict["input_battery"] = st.number_input(
             label="Battery in kWh",
             min_value=0,
             max_value=10000,
-            step=1000)
+            step=1)
 
         # input value for combined heat and power plant in kW(electric)
         input_values_dict["input_chp"] = st.number_input(
             label="Combined Heat and Power Plant in kW(el)",
             min_value=0,
             max_value=1000000,
-            step=1000)
+            step=1)
 
         # input value for ground coupled heat pump
         input_values_dict["input_gchp"] = st.number_input(
             label="Heat Pump in kW",
             min_value=0,
             max_value=5000,
-            step=1000)
+            step=1)
 
         # # input value for central thermal storage
         # input_values_dict["input_cts"] = st.number_input(
@@ -98,7 +107,7 @@ def dt_input_sidebar():
             label="Thermal Storage (decentral) in kWh",
             min_value=0,
             max_value=10000,
-            step=1000)
+            step=1)
 
         # bool if DH network should be active
         input_dh = st.checkbox(
@@ -121,7 +130,7 @@ def dt_input_sidebar():
     return None
 
 
-def execute_sesmg_demo(demo_file, demo_results):
+def execute_sesmg_demo(demo_file, demo_results, mode):
     """
         Excecutes the optimization algorithm.
 
@@ -130,7 +139,13 @@ def execute_sesmg_demo(demo_file, demo_results):
         :param demo_results:
         :type demo_results:
     """
-
+    
+    # activate criterion switch if run is optimized to its emissions
+    if mode == "emissions":
+        criterion_switch = True
+    else:
+        criterion_switch = False
+        
     # run sesmg main function with reduced / fixed input options
     sesmg_main(
         scenario_file=demo_file,
@@ -138,7 +153,7 @@ def execute_sesmg_demo(demo_file, demo_results):
         num_threads=1,
         timeseries_prep=["None", "None", "None", "None", 0],
         graph=False,
-        criterion_switch=False,
+        criterion_switch=criterion_switch,
         xlsx_results=False,
         console_results=False,
         solver="cbc",
@@ -155,22 +170,13 @@ def create_demo_model_definition(mode):
     """
         Modifies demo model definition.
 
-        :param mode: ????????????
+        :param mode: optimization criterion which is chosen in the GUI
         :type mode: str
-        :param mode: ????????????
-        :type mode: dict
     """
-
-    # define main path to SESMG program files folder
-    mainpath_pf = os.path.dirname(os.path.dirname(
-        os.path.dirname(__file__)))
-    # define main path to SESMG main folder
-    mainpath_mf = os.path.dirname(os.path.dirname(
-        os.path.dirname(os.path.dirname(__file__))))
 
     xfile = openpyxl.load_workbook(
         mainpath_pf
-        + "/Demo_Tool/v0.4.0_demo_model_definition/"
+        + "/demo_tool/v0.4.0_demo_model_definition/"
         + mode_dict.get(mode)[0],
         data_only=True)
 
@@ -212,6 +218,13 @@ def create_demo_model_definition(mode):
     sheet = xfile["links"]
     sheet["C3"] = input_values_dict["input_dh"]
 
+    # check if /demo exists in results direcotry
+    if os.path.join(mainpath_mf, "results", "demo") \
+        not in glob.glob(os.path.join(os.path.dirname(os.path.dirname(
+            os.path.dirname(os.path.dirname(__file__)))), "results", "*")):
+        # create /results/demo directory
+        os.mkdir(path=os.path.join(mainpath_mf, "results", "demo"))
+        
     # safe motified xlsx file in the results/demo folder
     xfile.save(
         mainpath_mf
@@ -222,7 +235,8 @@ def create_demo_model_definition(mode):
     execute_sesmg_demo(
         demo_file=mainpath_mf + mode_dict.get(mode)[1]
         + r"/model_definition.xlsx",
-        demo_results=mainpath_mf + mode_dict.get(mode)[1])
+        demo_results=mainpath_mf + mode_dict.get(mode)[1],
+        mode=input_values_dict["input_criterion"])
 
 
 def show_demo_run_results(mode):
@@ -230,19 +244,13 @@ def show_demo_run_results(mode):
         Loading and displaying demo run results.
     """
 
-    # define main path to SESMG main folder
-    mainpath_mf = os.path.dirname(os.path.dirname(
-        os.path.dirname(os.path.dirname(__file__))))
-
     # load summary.csv from results/demo /emissions or /monetary folder
     # which was replaced with the model run above
     df_summary = pd.read_csv(
         mainpath_mf
         + mode_dict.get(mode)[1]
         + r"/summary.csv")
-
     summary_headers = list(df_summary)
-    st.write(df_summary)
 
     # change dimension of the values to Mio.€/a and t/a
     annual_costs = float(df_summary[summary_headers[3]] / 1000000)
@@ -273,39 +281,38 @@ def show_demo_run_results(mode):
         label="Annual Costs in Mil. €",
         value=round(annual_costs, 2),
         delta=rel_result_costs,
-        delta_color="inverse"
-        )
+        delta_color="inverse")
     cost2.metric(
         label="Annual Costs in t",
         value=round(annual_emissions, 2),
         delta=rel_result_emissions,
-        delta_color="inverse"
-        )
+        delta_color="inverse")
 
 
 def demo_start_page():
     """
         Start page text for the demo tool.
     """
+    # import markdown text from GUI files
+    read_markdown_document(
+        document_path="docs/GUI_texts/demo_tool.md",
+        folder_path="")
 
-    st.header("Welcome using the Demo Tool of the Spreadsheet Energy System \
-              Model Generator (SESMG)")
-    st.subheader("Demo Energy System:")
-    st.write("In this DEMO the financial costs and carbon dioxide emissions \
-             of a residential area are simulated. For improvement, the \
-             technologies listed below are available with the parameters \
-             below. The simulated scenarios can be compared with the status \
-             quo, the financial minimum and the emission minimum.")
-    st.write("The urban area of a fictitious city to be optimized comprises \
-             15 000 inhabitants. The annual heat demand of 52 000 MWh/a is \
-             currently provided by 24 MW gas heating systems. Annually, \
-             14 000 MWh of electricity are purchased via the electricity \
-             wholesale market.")
-    st.write("Which of the technologies on the left would you incorporate and \
-             how would you dimension them into the energy system to minimize \
-             the current cost of 10.7 Mil. €/a and/or CO2 emissions of \
-             17 200 t/a?")
-    st.write("Type in your solution on the left side and get it simulated!")
+
+def demo_start_page_graph():
+    """
+        Imports graph to be displayed in the start page.
+    """
+    # import demo tool graph
+    image = Image.open(
+        os.path.join(os.path.dirname(os.path.dirname(
+            os.path.dirname(os.path.abspath(__file__)))),
+            "demo_tool",
+            "v0.4.0_demo_model_definition",
+            "demo_system_graph.png"))
+
+    # show image
+    st.image(image)
 
 
 def demo_parameters_page():
@@ -315,16 +322,22 @@ def demo_parameters_page():
 
 # TODO: Update to actual values and drop not used elements & unify wording!
 
-    model_demands = [
-        ["Electricity", "14 000 000 kWh/a", "h0 Load Profile"],
-        ["Heat", "52 203 000 kWh/a", "EFH Load Profile"]
-        ]
+    # model_demands = [
+    #     ["Electricity", "14 000 000 kWh/a", "h0 Load Profile"],
+    #     ["Heat", "52 203 000 kWh/a", "EFH Load Profile"]]
 
-    model_prices = [
-        ["Gas Import", "6.29 ct/kWh", "?"],
-        ["Electricity Import", "31.22 ct/kWh", "366 g/kWh"],
-        ["Electricity Export", "- 6.8 ct/kWh", "- 27 g/kWh"]
-        ]
+    # model_prices = [
+    #     ["Gas Import", "6.29 ct/kWh", "?"],
+    #     ["Electricity Import", "31.22 ct/kWh", "366 g/kWh"],
+    #     ["Electricity Export", "- 6.8 ct/kWh", "- 27 g/kWh"]]
+    
+    model_demands_price = [
+        ["Electricity", "14 000 000 kWh/a", "h0 Load Profile",
+         "Gas Import", "6.29 ct/kWh", "?"],
+        ["Heat", "52 203 000 kWh/a", "EFH Load Profile",
+         "Electricity Import", "31.22 ct/kWh", "366 g/kWh"],
+        ["", "", "",
+         "Electricity Export", "- 6.8 ct/kWh", "- 27 g/kWh"]]
 
     model_parameter = [
         # ['Windturbines': '2 000 000 €/MW, 8 g/kWh, 20 a, max. 29.7 MW'],
@@ -351,16 +364,21 @@ def demo_parameters_page():
         # ["Air Source Heat Pump", "1 318 000 €/MW", "12g/kWh", "18 a"],
     ]
 
-    stdf1, stdf2 = st.columns(2)
-    # display dataframe for model demands
-    stdf1.dataframe(data=pd.DataFrame(
-        data=model_demands,
-        columns=["Energy Form", "Demand", "Usage Pattern"]))
+    # stdf1, stdf2 = st.columns(2)
+    # # display dataframe for model demands
+    # stdf1.dataframe(data=pd.DataFrame(
+    #     data=model_demands,
+    #     columns=["Energy Form", "Demand", "Usage Pattern"]))
 
-    # display dataframe for specific import & export cots
-    stdf2.dataframe(data=pd.DataFrame(
-        data=model_prices,
-        columns=["Energy Form", "Specific Costs", "Specific Emissions"]))
+    # # display dataframe for specific import & export cots
+    # stdf2.dataframe(data=pd.DataFrame(
+    #     data=model_prices,
+    #     columns=["Energy Form", "Specific Costs", "Specific Emissions"]))
+    
+    st.dataframe(data=pd.DataFrame(
+        data=model_demands_price,
+        columns=["Energy Demand", "Demand", "Usage Pattern",
+                 "Energy Form", "Specific Costs", "Specific Emissions"]))
 
     # display dataframe for technology parameter
     st.dataframe(data=pd.DataFrame(
@@ -377,7 +395,6 @@ def change_state_submitted_demo_run():
     """
     st.session_state["state_submitted_demo_run"] = "done"
 
-
 # run demo application
 # initialize global streamlit settings
 st_settings_global()
@@ -385,11 +402,17 @@ st_settings_global()
 # run demotool input sidebar
 input_values_dict = dt_input_sidebar()
 
-# creating main demo tool page
+# creating main demo tool
 # loading start page
 demo_start_page()
-# loading parameter overview
-demo_parameters_page()
+# set columns
+democol1, democol2 = st.columns(spec=[3, 1.5])
+with democol1:
+    # loading parameter overview
+    demo_parameters_page()
+with democol2:
+    # loading system graph
+    demo_start_page_graph()
 
 # show results after submit button was clicked
 if st.session_state["state_submitted_demo_run"] == "done":
