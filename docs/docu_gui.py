66b1191476f7981111268631e7d5ccb921402732
Becker <gb611137@fh-muenster.de>
2023-04-17 09:46:00 +0200

    docu(API-documentation): correct path

--- docs/docu_gui.py
+++ docs/docu_gui.py
@@ -2,5 +2,5 @@ import subprocess
 import os
 
 subprocess.call("streamlit run {}".format(
-                os.path.dirname(__file__) +
+                os.path.dirname(os.path.dirname(__file__)) +
                 "/program_files/GUI_st/1_Main_Application.py"), shell=True)
