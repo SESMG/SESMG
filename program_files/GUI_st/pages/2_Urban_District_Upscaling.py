f7c39213e9bbf8154916c7745b6af0e98132ae3f
jtock <jan.tockloth@fh-muenster.de>
2023-05-08 19:20:17 +0200

    code(GUI_udu): remove unused code, add code documentation

--- program_files/GUI_st/pages/2_Urban_District_Upscaling.py
+++ program_files/GUI_st/pages/2_Urban_District_Upscaling.py
@@ -18,7 +18,11 @@ from program_files.GUI_st.GUI_st_global_functions \
 st_settings_global()
 
 
-def us_application():
+def us_application() -> None:
+    """
+        Definition of the sidebar elements for the urban district
+        upscaling page and starting a udu tool run.
+    """
 
     # Import GUI help comments from the comment json and safe as an dict
     GUI_helper = import_GUI_input_values_json(
@@ -26,6 +30,8 @@ def us_application():
         + "/GUI_st_help_comments.json")
 
     model_definition_df = ""
+
+    # create form-submit element for multiple inputs
     with st.sidebar.form("Input Parameters"):
 
         # input us sheet
@@ -38,6 +44,7 @@ def us_application():
             label="Import your standard parameter sheet:",
             help=GUI_helper["udu_fu_sp_sheet"])
 
+        # text input to define the file name
         result_file_name = \
             st.text_input(label="Type in your model definition file name.",
                           help=GUI_helper["udu_ti_model_def_name"])
@@ -45,7 +52,6 @@ def us_application():
         # Submit button to start optimization.
         submitted_us_run = st.form_submit_button(
                 label="Start US Tool",
-                on_click=creating_xlsx,
                 help=GUI_helper["udu_fs_start_US_tool"])
 
         # Run program main function if start button is clicked
@@ -69,19 +75,23 @@ def us_application():
                         clustering=False,
                         clustering_dh=False)
 
+    # create download button
     st.sidebar.download_button(label="Download your model definition",
                                data=model_definition_df,
                                file_name=result_file_name + ".xlsx",
                                help=GUI_helper["udu_b_download_model_def"])
 
+    # define urban district upscaling model definition as session state
     st.session_state["state_model_definition"] = model_definition_df
-    st.session_state["state_xlsx"] = "done"
 
 
-def standard_page():
+def standard_page() -> None:
     """
-        Load the existing file of the us tool description and include grafics.
+        Load the existing file of the us tool description and include
+        graphics.
     """
+
+    # import of text and graphic
     reduced_readme = read_markdown_document(
         document_path="docs/GUI_texts/us_tool.md",
         folder_path=f'{"docs/images/manual/UpscalingTool/*"}')
@@ -92,34 +102,33 @@ def standard_page():
 
 
 # second column
-def after_processing_page():
+def udu_preprocessing_page() -> None:
+    """
+        Definition of the page elements after the urban district
+        upscaling tool ran.
+    """
+
+    # define header
     st.header("Model defintion")
 
-    if "state_xlsx" not in st.session_state:
-        st.session_state["state_xlsx"] = "not done"
-    if st.session_state["state_xlsx"] == "done":
-        xls = pd.ExcelFile(st.session_state["state_model_definition"])
-        tabs = xls.sheet_names
-        # without info column
-        tabs.pop(0)
-        tab_bar = st.tabs(tabs)
-        for i in range(len(tabs)):
-            with tab_bar[i]:
-                df_input = xls.parse(tabs[i])
-                df_input = df_input.astype(str)
-                st.dataframe(df_input)
+    # create model defnition table
+    xls = pd.ExcelFile(st.session_state["state_model_definition"])
+    tabs = xls.sheet_names
+    # without info column
+    tabs.pop(0)
+    tab_bar = st.tabs(tabs)
+    for i in range(len(tabs)):
+        with tab_bar[i]:
+            df_input = xls.parse(tabs[i])
+            df_input = df_input.astype(str)
+            st.dataframe(df_input)
 
 
-def change_xlsx_state():
-    st.session_state["state_xlsx"] = "done"
-
-
-def creating_xlsx():
-    st.session_state["state_created"] = "done"
-
-
+# running sidebar elements
 us_application()
+# running the start / main page if tool did not run yet
 if st.session_state["state_model_definition"] == "":
     standard_page()
+# running preprocessing page if tool ran
 else:
-    after_processing_page()
+    udu_preprocessing_page()
