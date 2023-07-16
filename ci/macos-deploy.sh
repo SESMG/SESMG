e78e99936be114afbd29ecc385ef3eb92315f0f6
Gregor Becker <gregor.becker@fh-muenster.de>
2023-07-16 22:29:05 +0200

    Update macos-deploy.sh

--- ci/macos-deploy.sh
+++ ci/macos-deploy.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-python3 ci/macos_app_arch_check.py
-
-python3 ci/macos_app_adhoc_sign.py
-
 pip3 install dmgbuild
 
 python3 -m dmgbuild -s ci/appdmg.py "SESMG" dist_actions/SESMG_${VER}.dmg
