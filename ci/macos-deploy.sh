f05586386d70bc2ff99db66a50bee44cb4807093
Gregor Becker <gregor.becker@fh-muenster.de>
2023-07-31 12:47:47 +0200

    Delete ci directory

--- ci/macos-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pip3 install dmgbuild
-
-python3 -m dmgbuild -s ci/appdmg.py "SESMG" dist_actions/SESMG_${VER}.dmg
--- ci/macos_app_adhoc_sign.py
+++
@@ -1,5 +0,0 @@
-from py2app.util import codesign_adhoc
-
-APPDIR = 'dist/SESMG.app'
-
-codesign_adhoc(APPDIR)
