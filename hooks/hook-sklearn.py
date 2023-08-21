346a7ff432b76c7491ab9dd727df3a5c2e8e496a
Gregor Becker <gregor.becker@fh-muenster.de>
2023-08-21 07:43:11 +0200

    code(sklearn_hook): remove bug

--- hooks/hook-sklearn.py
+++ hooks/hook-sklearn.py
@@ -3,4 +3,4 @@ from PyInstaller.utils.hooks import collect_data_files
 hiddenimports = collect_submodules('sklearn')
 
 datas = collect_data_files('sklearn', include_py_files=True)
-datas += collect_data_files('sklearn.extra', include_py_files=True)
+datas += collect_data_files('sklearn_extra', include_py_files=True)
