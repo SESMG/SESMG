6a3e47b5890e831bce982d7f355f421b5dd6495a
Gregor Becker <gregor.becker@fh-muenster.de>
2023-08-21 14:31:53 +0200

    code(hook-sklearn): add dependency of sklearn

--- hooks/hook-sklearn.py
+++ hooks/hook-sklearn.py
@@ -4,3 +4,4 @@ hiddenimports = collect_submodules('sklearn')
 
 datas = collect_data_files('sklearn', include_py_files=True)
 datas += collect_data_files('sklearn_extra', include_py_files=True)
+datas += collect_data_files('joblib', include_py_files=True)
