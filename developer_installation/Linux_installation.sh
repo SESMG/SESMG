6489583aeac5097819905e1d2ee063d7058c48b0
GregorBecker <gregor.becker@fh-muenster.de>
2024-02-28 09:29:02 +0100

    installation(MacOS, Linux): Try to prevent the user from using the wrong Python version.
    Linux has to be tested, Windows is missing #205

--- developer_installation/Linux_installation.sh
+++ developer_installation/Linux_installation.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+function exists_in_list() {
+	# mask the user given inputs
+    LIST=$1
+    DELIMITER=$2
+    VALUE=$3
+	# replace delimiter by whitespaces
+    LIST_WHITESPACES=`echo $LIST | tr "$DELIMITER" " "`
+	# iterate threw the list to check for given Version 
+    for x in $LIST_WHITESPACES; do
+        if [ "$x" = "$VALUE" ]; then
+			# return 0 if version was found
+            return 0
+        fi
+    done
+	# return 1 if version was not found
+    return 1
+}
 
 echo --------------------------------
 echo Installation started
@@ -11,8 +28,17 @@ echo --------------------------------
 chmod a+rwx "$(pwd)"
 pip3 install virtualenv
 
-echo "Enter your Python Version here: "
+echo "Enter yout Python Version here: "
 read PYVERSION
+
+list_of_allowed_versions="3.8,3.9,3.10,3.11"
+
+if exists_in_list "$list_of_allowed_versions" "," $PYVERSION; then
+    echo "Version Input allowed, installation will continue."
+else
+    echo "Input not in the list of allowed Versions restart the installation with an allowed version." 1>&2 
+	exit 1 
+fi
 
 cd "$(pwd)"
 cd ..
@@ -23,13 +49,12 @@ echo ---------------------------------------------
 echo download and install required python packages
 echo ---------------------------------------------
 
-
-venv/bin/pip3 install -r ./requirements.txt
+pip3 install -r ./requirements.txt
 
 echo ----------------------
 echo Installation completed
 echo ----------------------
-venv/bin/python3 -m streamlit run "program_files/GUI_st/1_Main_Application.py"
+python3 -m streamlit run "program_files/GUI_st/1_Main_Application.py"
 
 exit
 
