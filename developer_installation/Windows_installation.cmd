7d7d7f8f24db19cf407a747adf614e9a028392ac
jtock <jan.tockloth@fh-muenster.de>
2024-07-09 08:52:24 +0200

    installation(windows): add warning to prevent wrong python version

--- developer_installation/Windows_installation.cmd
+++ developer_installation/Windows_installation.cmd
@@ -1,35 +1,55 @@
 @echo ################################
-@echo Building the Virtual Enviroment
+@echo Building the Virtual Environment
 @echo File path: "%~dp0"
 @echo ################################
 
 @echo off
+setlocal enabledelayedexpansion
+
+set "list_of_allowed_versions=3.8,3.9,3.10,3.11"
 set /p PYVERSION="Enter your Python Version here:"
-cd "%~p0 "
-cd ..
-py -%PYVERSION% -m venv venv
 
-cd venv/Scripts/
-start /b activate.bat
+set "found=0"
+for %%v in (%list_of_allowed_versions%) do (
+    if "%%v"=="%PYVERSION%" (
+        set "found=1"
+        goto :found
+    )
+)
 
-pip install pipwin
-pipwin install gdal
-pipwin install fiona
+:found
+if "%found%"=="1" (
+    echo Version Input allowed, installation will continue.
+    
+    cd "%~dp0"
+    cd ..
+    py -%PYVERSION% -m venv venv
 
-mkdir %userprofile%\.streamlit
-move ..\.streamlit\credentials.toml %userprofile%\.streamlit\credentials.toml
+    cd venv\Scripts\
+    start /b activate.bat
 
-@echo #############################################
-@echo download and install required python packages
-@echo #############################################
+    pip install pipwin
+    pipwin install gdal
+    pipwin install fiona
 
-pip install -r ../../requirements.txt
+    mkdir %userprofile%\.streamlit
+    move ..\..\..\.streamlit\credentials.toml %userprofile%\.streamlit\credentials.toml
 
-@echo ######################
-@echo Installation completed
-@echo Starting SESMG
-@echo ######################
-cd ..
-cd ..
-venv\Scripts\python.exe -m streamlit run "program_files/GUI_st/1_Main_Application.py"
+    @echo #############################################
+    @echo download and install required python packages
+    @echo #############################################
 
+    pip install -r ../../requirements.txt
+
+    @echo ######################
+    @echo Installation completed
+    @echo Starting SESMG
+    @echo ######################
+    
+    cd ..
+    cd ..
+    venv\Scripts\python.exe -m streamlit run "program_files/GUI_st/1_Main_Application.py"
+) else (
+    echo Input not in the list of allowed Versions. Restart the installation with an allowed version.
+    exit /b 1
+)
