d700fc5482b8f77a4c5c75b46bb73423468f6e00
FranziKoert <fk098542@fh-muenster.de>
2024-10-30 16:12:52 +0100

    Change:create_results

--- program_files/postprocessing/create_results.py
+++ program_files/postprocessing/create_results.py
@@ -253,6 +253,9 @@ class Results:
             ],
         )
 
+        # export the list of components for further result calculation
+        self.components_list = loc
+
         # Dataframes are exported as csv for further processing
         loc.to_csv(result_path + "/components.csv", index=False)
 
