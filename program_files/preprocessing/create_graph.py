b276f611c51d3a4e77a6cbbc93ff8296ac8d5753
Oquiro <oq985110@fh-muenster.de>
2025-08-13 18:08:47 +0200

    Update create_graph.py (Graphic solutions)

--- program_files/preprocessing/create_graph.py
+++ program_files/preprocessing/create_graph.py
@@ -61,6 +61,9 @@ class ESGraphRenderer:
 
         os.environ["PATH"] += os.pathsep + "C:\\Program Files (x86)\\Graphviz2.38\\bin"
         self.dot = graphviz.Digraph(format="png")
+        self._nodes_added = set()
+        self._edges_added = set()
+        self._edge_pairs_added = set()
 
         if legend is True:
             with self.dot.subgraph(name="cluster_1") as self.c:
@@ -91,8 +94,11 @@ class ESGraphRenderer:
         for nd in energy_system.nodes:
             # make sur the label is a string and not a tuple
             if isinstance(nd, Bus) and not "infrastructure" in nd.label:
-                self.add_comp(str(nd.label), "ellipse", False, self.dot)
-                # keep the bus reference for drawing edges later
+                _lbl = str(nd.label).lower()
+                if _lbl in {'dh-network', 'dh-network-exergy', 'dh-network-anergy'}:
+                    continue
+                else:
+                    self.add_comp(str(nd.label), 'ellipse', False, self.dot)
                 self.busses.append(nd)
             elif isinstance(nd, Bus) and "infrastructure" in nd.label:
                 pass
@@ -114,18 +120,28 @@ class ESGraphRenderer:
             isinstance(nd, HeatPipeline) and 'anergy' in str(nd.label).lower()
             for nd in energy_system.nodes
         )
+        heatpipe_present = any(
+            isinstance(nd, HeatPipeline)
+            for nd in energy_system.nodes
+        )
+        
         if exergy_present and anergy_present:
             net_labels = ['dh-network-exergy', 'dh-network-anergy']
 
-        elif exergy_present or anergy_present:
-            # only one or anyone -> generc label
-            net_labels = ['dh-network'] 
+        elif exergy_present:
+            net_labels = ['dh-network-exergy']
+        elif anergy_present:
+            net_labels = ['dh-network-anergy']
+        elif heatpipe_present:
+            # Generic case for heat pipelines
+            net_labels = ['dh-network']
         else:
             net_labels = []
-        # Adding one node only if there is no heat pipeline
+        # Create heat-network node(s) as needed
         for net in net_labels:
-            if net not in str(self.dot):
-                self.dot.node(net, net, shape='hexagon')
+            if net not in self._nodes_added:
+                self.dot.node(net, linebreaks(net), shape='hexagon')
+                self._nodes_added.add(net)
 
         # draw the edges between the nodes based on each bus inputs/outputs
         for bus in self.busses:
@@ -139,6 +155,9 @@ class ESGraphRenderer:
         self.render(filepath, view)
 
     def add_comp(self, label, shape, dashed, subgraph):
+        label_str = str(label)
+        if label_str in self._nodes_added:
+            return
         subgraph.node(
             linebreaks(label),
             shape=shape,
@@ -148,6 +167,7 @@ class ESGraphRenderer:
             height="0.7",
             style="dashed" if dashed else "",
         )
+        self._nodes_added.add(label_str)
 
     def connect(self, a, b):
         """Draw an arrow from node a to node b
@@ -165,7 +185,14 @@ class ESGraphRenderer:
         
         # If one is a Heatpipeline, we connect it to the network
         if isinstance(a, HeatPipeline) or isinstance(b, HeatPipeline):
-            # We chose the pipeline based on the label
+            
+            if not hasattr(self, '_edges_added'):
+                self._edges_added = set()
+            if not hasattr(self, '_edge_pairs_added'):
+                self._edge_pairs_added = set()
+            if not hasattr(self, '_nodes_added'):
+                self._nodes_added = set()
+
             pipeline = b if isinstance(b, HeatPipeline) else a
             plc_label = str(pipeline.label).lower()
             if 'exergy' in plc_label:
@@ -175,15 +202,29 @@ class ESGraphRenderer:
             else:
                 net = 'dh-network'
             
-            # Now we check if the other is a pipeline or not
             if isinstance(b, HeatPipeline):
-                self.dot.edge(a_label, net)
+                # BUS → NET
+                pair_key = frozenset({a_label, net})
+                if pair_key not in self._edge_pairs_added:
+                    self._edge_pairs_added.add(pair_key)
+                    if (a_label, net) not in self._edges_added:
+                        self.dot.edge(a_label, net)
+                        self._edges_added.add((a_label, net))
             else:
-                self.dot.edge(net, b_label)
-
+                # NET → BUS
+                pair_key = frozenset({net, b_label})
+                if pair_key not in self._edge_pairs_added:
+                    self._edge_pairs_added.add(pair_key)
+                    if (net, b_label) not in self._edges_added:
+                        self.dot.edge(net, b_label)
+                        self._edges_added.add((net, b_label))
+            return
+        
         # If both are not Heatpipelines, we connect them normally
         elif not (isinstance(a, HeatPipeline) and isinstance(b, HeatPipeline)):
-            self.dot.edge(a_label, b_label)
+            if (a_label, b_label) not in self._edges_added:
+                self.dot.edge(a_label, b_label)
+                self._edges_added.add((a_label, b_label))
 
     def render(self, filepath, show):
         """Call the render method of the DiGraph instance"""
