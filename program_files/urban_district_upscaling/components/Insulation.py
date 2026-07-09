de755371839e4237e237340a60ff1477161f6ff8
FranziKoert <134854276+FranziKoert@users.noreply.github.com>
2026-07-09 14:52:01 +0200

    Add option to deactivate insulation in the upscaling sheet
    before the insulation was created for all buildings automatically, now there is an additional column the user can fill in to select insulation or not

--- program_files/urban_district_upscaling/components/Insulation.py
+++ program_files/urban_district_upscaling/components/Insulation.py
@@ -29,87 +29,89 @@ def create_building_insulation(building: dict, sheets: dict,
     """
     from program_files import append_component
 
-    yoc_roof = building["year of construction roof"]
-    yoc_wall = building["year of construction wall"]
-    yoc_window = building["year of construction windows"]
-    roof = building["rooftype"]
-    yoc_component = [yoc_roof, yoc_wall, yoc_window]
-    yoc_component_new = [yoc_component[i] if yoc_component[i] > 1918
-                         else "<1918" for i in range(len(yoc_component))]
-    building_component = ["roof", "outer wall", "window"]
+    # check if the insulation is activated
+    if building["insulation"] not in ["no", "No", "0"]:
+        yoc_roof = building["year of construction roof"]
+        yoc_wall = building["year of construction wall"]
+        yoc_window = building["year of construction windows"]
+        roof = building["rooftype"]
+        yoc_component = [yoc_roof, yoc_wall, yoc_window]
+        yoc_component_new = [yoc_component[i] if yoc_component[i] > 1918
+                             else "<1918" for i in range(len(yoc_component))]
+        building_component = ["roof", "outer wall", "window"]
 
-    standard_param = standard_parameters.parse("7_insulation")
-    standard_param.set_index("year of construction", inplace=True)
+        standard_param = standard_parameters.parse("7_insulation")
+        standard_param.set_index("year of construction", inplace=True)
 
-    u_values = {}
+        u_values = {}
 
-    for yoc, comp in zip(yoc_component_new, building_component):
-        u_values.update(
-            {
-                comp: [
-                    standard_param.loc[yoc][comp],
-                    standard_param.loc["potential"][comp],
-                    standard_param.loc["periodical costs"][comp],
-                    standard_param.loc["periodical constraint costs"][comp],
+        for yoc, comp in zip(yoc_component_new, building_component):
+            u_values.update(
+                {
+                    comp: [
+                        standard_param.loc[yoc][comp],
+                        standard_param.loc["potential"][comp],
+                        standard_param.loc["periodical costs"][comp],
+                        standard_param.loc["periodical constraint costs"][comp],
+                    ]
+                }
+            )
+            if comp == "roof":
+                u_values[comp] += [
+                    standard_param.loc["potential flat"]["roof"],
+                    standard_param.loc["periodical costs flat"]["roof"],
+                    standard_param.loc["periodical constraint costs flat"]["roof"],
                 ]
-            }
-        )
-        if comp == "roof":
-            u_values[comp] += [
-                standard_param.loc["potential flat"]["roof"],
-                standard_param.loc["periodical costs flat"]["roof"],
-                standard_param.loc["periodical constraint costs flat"]["roof"],
-            ]
-    param_dict = {
-        "active": 1,
-        "existing": 0,
-        "sink": str(building["label"]) + "_heat_demand",
-        "temperature indoor": 20,
-        "heat limit temperature": 15,
-    }
-    if building["area windows"]:
-        window_dict = param_dict.copy()
-        window_dict.update(
-            {
-                "label": str(building["label"]) + "_window",
-                "U-value old": u_values["window"][0],
-                "U-value new": u_values["window"][1],
-                "area": building["area windows"],
-                "periodical costs": u_values["window"][2],
-                "periodical constraint costs": u_values["window"][3],
-            }
-        )
-        sheets = append_component(sheets, "insulation", window_dict)
+        param_dict = {
+            "active": 1,
+            "existing": 0,
+            "sink": str(building["label"]) + "_heat_demand",
+            "temperature indoor": 20,
+            "heat limit temperature": 15,
+        }
+        if building["area windows"]:
+            window_dict = param_dict.copy()
+            window_dict.update(
+                {
+                    "label": str(building["label"]) + "_window",
+                    "U-value old": u_values["window"][0],
+                    "U-value new": u_values["window"][1],
+                    "area": building["area windows"],
+                    "periodical costs": u_values["window"][2],
+                    "periodical constraint costs": u_values["window"][3],
+                }
+            )
+            sheets = append_component(sheets, "insulation", window_dict)
 
-    if building["area outer wall"]:
-        wall_dict = param_dict.copy()
-        wall_dict.update(
-            {
-                "label": str(building["label"]) + "_wall",
-                "U-value old": u_values["outer wall"][0],
-                "U-value new": u_values["outer wall"][1],
-                "area": building["area outer wall"],
-                "periodical costs": u_values["outer wall"][2],
-                "periodical constraint costs": u_values["outer wall"][3],
-            }
-        )
-        sheets = append_component(sheets, "insulation", wall_dict)
+        if building["area outer wall"]:
+            wall_dict = param_dict.copy()
+            wall_dict.update(
+                {
+                    "label": str(building["label"]) + "_wall",
+                    "U-value old": u_values["outer wall"][0],
+                    "U-value new": u_values["outer wall"][1],
+                    "area": building["area outer wall"],
+                    "periodical costs": u_values["outer wall"][2],
+                    "periodical constraint costs": u_values["outer wall"][3],
+                }
+            )
+            sheets = append_component(sheets, "insulation", wall_dict)
 
-    if building["area roof"]:
-        u_value_new = u_values["roof"][4 if roof == "flat roof" else 1]
-        periodical_costs = u_values["roof"][5 if roof == "flat roof" else 2]
-        periodical_constr = u_values["roof"][3 if roof != "flat roof" else 6]
-        roof_dict = param_dict.copy()
-        roof_dict.update(
-            {
-                "label": str(building["label"]) + "_roof",
-                "U-value old": u_values["roof"][0],
-                "U-value new": u_value_new,
-                "area": building["area roof"],
-                "periodical costs": periodical_costs,
-                "periodical constraint costs": periodical_constr,
-            }
-        )
-        sheets = append_component(sheets, "insulation", roof_dict)
+        if building["area roof"]:
+            u_value_new = u_values["roof"][4 if roof == "flat roof" else 1]
+            periodical_costs = u_values["roof"][5 if roof == "flat roof" else 2]
+            periodical_constr = u_values["roof"][3 if roof != "flat roof" else 6]
+            roof_dict = param_dict.copy()
+            roof_dict.update(
+                {
+                    "label": str(building["label"]) + "_roof",
+                    "U-value old": u_values["roof"][0],
+                    "U-value new": u_value_new,
+                    "area": building["area roof"],
+                    "periodical costs": periodical_costs,
+                    "periodical constraint costs": periodical_constr,
+                }
+            )
+            sheets = append_component(sheets, "insulation", roof_dict)
 
     return sheets
